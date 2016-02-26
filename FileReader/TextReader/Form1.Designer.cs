ddc759aa8be975deb2b71cc0698e39e26aefb517
kennethCarl <kennethcarlybanez@gmail.com>
2016-02-26 08:45:25 +0800

    pdf text reader

---
+++ FileReader/TextReader/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace TextReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNumberOfLines = new System.Windows.Forms.Label();
+            this.lblNumberOfWords = new System.Windows.Forms.Label();
+            this.lblNumberOfPages = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(47, 20);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(262, 20);
+            this.txtPath.TabIndex = 8;
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(317, 20);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 21);
+            this.btnReadFile.TabIndex = 2;
+            this.btnReadFile.Text = "Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number of Words:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Number of Lines:";
+            // 
+            // lblNumberOfLines
+            // 
+            this.lblNumberOfLines.AutoSize = true;
+            this.lblNumberOfLines.Location = new System.Drawing.Point(114, 74);
+            this.lblNumberOfLines.Name = "lblNumberOfLines";
+            this.lblNumberOfLines.Size = new System.Drawing.Size(13, 13);
+            this.lblNumberOfLines.TabIndex = 7;
+            this.lblNumberOfLines.Text = "0";
+            // 
+            // lblNumberOfWords
+            // 
+            this.lblNumberOfWords.AutoSize = true;
+            this.lblNumberOfWords.Location = new System.Drawing.Point(114, 96);
+            this.lblNumberOfWords.Name = "lblNumberOfWords";
+            this.lblNumberOfWords.Size = new System.Drawing.Size(13, 13);
+            this.lblNumberOfWords.TabIndex = 6;
+            this.lblNumberOfWords.Text = "0";
+            // 
+            // lblNumberOfPages
+            // 
+            this.lblNumberOfPages.AutoSize = true;
+            this.lblNumberOfPages.Location = new System.Drawing.Point(114, 51);
+            this.lblNumberOfPages.Name = "lblNumberOfPages";
+            this.lblNumberOfPages.Size = new System.Drawing.Size(13, 13);
+            this.lblNumberOfPages.TabIndex = 10;
+            this.lblNumberOfPages.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Number of Page:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 118);
+            this.Controls.Add(this.lblNumberOfPages);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblNumberOfLines);
+            this.Controls.Add(this.lblNumberOfWords);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Text Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNumberOfLines;
+        private System.Windows.Forms.Label lblNumberOfWords;
+        private System.Windows.Forms.Label lblNumberOfPages;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FileReader/TextReader/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+
+namespace TextReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            int numberOfLines = 0, numberOfWords = 0, numberOfPage = 0;
+            string[] file;
+            ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+            
+            file = this.txtPath.Text.Split('.');
+
+            if (!File.Exists(this.txtPath.Text))
+                MessageBox.Show("File doesn't exist.", "System Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (!file[file.Length - 1].Equals("pdf"))
+                MessageBox.Show("File should be in PDF format.", "System Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                using (PdfReader reader = new PdfReader(this.txtPath.Text))
+                {
+                    //StringBuilder text = new StringBuilder();
+                    numberOfPage = reader.NumberOfPages;
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        string thePage = PdfTextExtractor.GetTextFromPage(reader, i, its);
+                        char[] splitter = new char[] { ' ', ',', '/', '.', '\t' };
+                        string[] theLines = thePage.Split('\n');
+                        string[] theWords = thePage.Split(' ');
+
+                        numberOfLines = numberOfLines + theLines.Length;
+
+                        for (int j = 0; j < theWords.Length; j++)
+                        {
+                            if (!theWords[j].Equals(""))
+                            {
+                                byte[] asciiBytes = Encoding.ASCII.GetBytes(theWords[j]);
+                                bool flag = true;
+
+                                for(int k = 0; k < asciiBytes.Length; k++)
+                                {
+                                    //Check if letter
+                                    if (!((asciiBytes[k] >= 65 && asciiBytes[k] <= 90) || (asciiBytes[k] >= 97 && asciiBytes[k] <= 122)))
+                                    {
+                                        //If value is not a letter and found in last position of a word 
+                                        if (k != asciiBytes.Length - 1)
+                                        {
+                                            flag = false;
+                                            k = asciiBytes.Length;
+                                        }
+                                    }
+                                }
+
+                                if(flag)
+                                    numberOfWords++;
+                            }
+                                
+                        }
+                    }
+                }
+                this.lblNumberOfPages.Text = numberOfPage.ToString();
+                this.lblNumberOfLines.Text = numberOfLines.ToString();
+                this.lblNumberOfWords.Text = numberOfWords.ToString();
+            }  
+        }
+    }
+}
